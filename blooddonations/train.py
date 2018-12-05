015330e65e29a5dfbf41685c87e2a153bbed0af9
Michael Mann <mmann2943@gmail.com>
2018-12-04 22:23:03 -0500

    Added log loss as the metric and landed on .456 as the best iteration

--- blooddonations/train.py
+++ blooddonations/train.py
@@ -1,12 +1,17 @@
 import sys, os, time, random
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn import metrics
+from sklearn.metrics import log_loss
 import pandas as pd
 import numpy as np
 
 from features import Features
 
+#initialize variables
+maxtrainingtime = 1200 #20 * 60 = 20 minutes
+maxnumberofiterationloss = 20
+roundlevel = 3
+metrics = []
 
 # reproducable script
 random.seed(1)
@@ -58,27 +63,58 @@ for colname in classificationcols:
 
 
 rfc = RandomForestClassifier(
-    max_depth=None,
-    criterion="gini",
+    max_depth=10,
+    criterion="entropy",
     min_samples_leaf=30,
-    n_estimators=50,
-    n_jobs=-1
+    #n_estimators=50,
+    #n_jobs=-1
+    n_estimators=0,
+    warm_start=True
 )
 
 #fit the model to the data ie train the model with a simple learning loop
-rfc.fit(X_tr, Y_tr)
+n_iters = 0
+starttime = time.time()
+#execute the training loop
+while (True):
+    allworse = False
+    n_iters +=1
 
-#eval on the training set
-start = time.time()
-xv_yhat_proba = rfc.predict_proba(X_xv)
+    rfc.n_estimators = n_iters
+    
+    rfc.fit(X_tr, Y_tr)
+
+    #compute metric
+    tr_yhat_proba = rfc.predict_proba(X_tr)
+    tr_logloss = round(log_loss(Y_tr, tr_yhat_proba), roundlevel)
+    
+    print("iter {0} TRAIN LOG LOSS {1} PREV LOG LOSS {2}".format(n_iters, tr_logloss, (metrics[-1][1] - 0.0) if len(metrics) > 1 else 0.0))
+
+    #TODO: have we gotton worse in the last n iterations
+    if len(metrics) > maxnumberofiterationloss and tr_logloss >= metrics[-1][1]:        
+        # we have gotton worse compared to the previous iteration so lets see how bad we have gotton over time        
+        lastN = maxnumberofiterationloss * -1
+        allworse = all(tr_logloss >= x[1] for x in metrics[lastN:])
+        if allworse:
+            print("breaking out since last", maxnumberofiterationloss,"terations are worse or remained the same")
+
+    metrics.append((n_iters, tr_logloss))
+
+    exceededmaximumtimelimit = (time.time() - starttime) > maxtrainingtime
+    if allworse or exceededmaximumtimelimit:
+        print("exiting out of the learning loop due to early stopping")
+        break
+
+#get the best iteration of the model by sorting on the xv_rmse and n_iters    
+sorted_metrics = sorted(metrics, key=lambda x: (x[1], x[0]))
+best_iter, best_logloss = sorted_metrics[0][0], sorted_metrics[0][1]
+rfc.estimators_ = rfc.estimators_[:best_iter]
+
 tr_yhat_proba = rfc.predict_proba(X_tr)
-end = time.time()
-print("elapsed time of predict in seconds", end - start)
-
-#compute the auc over the prediction
-fpr, tpr, thresholds = metrics.roc_curve(Y_tr, tr_yhat_proba[:,1], pos_label=1)
-tr_auc = metrics.auc(fpr, tpr)
-print("TRAIN AUC {0}".format(tr_auc))
+tr_logloss = round(log_loss(Y_tr, tr_yhat_proba), roundlevel)
+    
+assert(tr_logloss == best_logloss)
+print("BEST ITER {0} LOG LOSS {1}".format(best_iter, best_logloss))
 
 #print out the feature importance
 importances = rfc.feature_importances_
